38b3e73774001a6005207cee97184251231d1453
Inmo Jang <inmo.jang@manchester.ac.uk>
2018-12-17 16:44:08 +0000

    Gripper Speed Control Added

--- robotiq_s_model_control/nodes/SModelController_vr.py
+++ robotiq_s_model_control/nodes/SModelController_vr.py
@@ -45,25 +45,28 @@ This serves as an example for publishing messages on the 'SModelRobotOutput' top
 import roslib; roslib.load_manifest('robotiq_s_model_control')
 import rospy
 from robotiq_s_model_control.msg import _SModel_robot_output  as outputMsg
+from robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
 from leap_motion.msg import Human_orion  as LeapMsg_orion # Newly defined LEAP ROS msg
 from rain_unity.msg import rain_system  as RainMsg # To receive the system status from Unity
 
 import sys
 import copy
 from time import sleep
+import numpy as np
+###############################################################
+###############################################################
+value_starting_to_close = 0.1 # Unit:Grab_strength (0-1)
+value_position_tolerance = 20 # Unit:0-255. If gPOA - rPRA is less than this value, then the gripper does not move
+value_for_max_speed = 125 # Unit:0-255. If gPOA - rPRA is more than this value, then the gripper moves in the fastest manner
+###############################################################
+###############################################################
+###############################################################
+###############################################################
 
-###############################################################
-###############################################################
-value_starting_to_close = 0.1
-
-###############################################################
-###############################################################
-###############################################################
-###############################################################
-
-global teleoperation_mode, LeapMsg_local, command, pub
+global teleoperation_mode, LeapMsg_local, GripperMsg_local, command, pub
 teleoperation_mode = ""
 LeapMsg_local = None
+GripperMsg_local = None
 
 def callback_Mode(status):
     global teleoperation_mode
@@ -73,11 +76,14 @@ def callback_Leap(status):
     global LeapMsg_local
     LeapMsg_local = status
 
+def callback_Gripper(status):
+    global GripperMsg_local
+    GripperMsg_local = status
 
 def control_gripper():
 
     global pub
-    global teleoperation_mode, LeapMsg_local
+    global teleoperation_mode, LeapMsg_local, GripperMsg_local
     global command
 
     publish_rate = 250
@@ -85,12 +91,12 @@ def control_gripper():
 
     while not rospy.is_shutdown():
 
-        if LeapMsg_local is not None and teleoperation_mode == "MODE_1":
+        if LeapMsg_local is not None and GripperMsg_local is not None and teleoperation_mode == "MODE_1":
             # Get Current Status
             leap_status = copy.deepcopy(LeapMsg_local)
 
             if (leap_status.left_hand.is_present is True):
-                # Control the Gripper
+                # Control the Gripper's position
                 grab_strength_from_unity = leap_status.left_hand.grab_strength
                 if (grab_strength_from_unity <= 1 and grab_strength_from_unity > value_starting_to_close): # Fully Close
                     # grab_strength = 255 # This is for the on-off controller
@@ -98,8 +104,26 @@ def control_gripper():
                 else: # Fully Open
                     grab_strength = 0 # Fully Open
                     
+                # Control the Gripper's speed
+                current_position = copy.deepcopy(GripperMsg_local.gPOA)
+                grap_speed_ = np.abs(grab_strength - current_position)
+   
+                # Additional treatment
+                command.rGTO = 1  
+                if grap_speed_ < value_position_tolerance:            
+                    grab_strength = current_position
+                    grap_speed = 0
+                    command.rGTO = 0 # Stops 
+                    
+                elif grap_speed_ > value_for_max_speed:
+                    grap_speed = 255
+                else:
+                    grap_speed = round((grap_speed_ - value_position_tolerance)*1.0/(value_for_max_speed-value_position_tolerance)*255,0)
 
+                print("Here Grapstrength: " + str(grab_strength) + " Speed : " + str(grap_speed) + "\n")
                 command.rPRA = grab_strength
+                command.rSPA = grap_speed
+
 
         pub.publish(command)
         rate.sleep()
@@ -132,7 +156,7 @@ def main(myArg1):
     
             command.rACT = 1 # Activation
             command.rGTO = 1 # 
-            command.rSPA = 255 # Opening/Closing Speed (Maximum = 255)
+            command.rSPA = 0 # Opening/Closing Speed (Maximum = 255)
             command.rICF = 0 # Individual Control Finger mode (1: Yes / 0: No)
             command.rFRA = 0 # Final grasping force (Maximum = 255). This does not matter in Gazebo.
 
@@ -148,6 +172,7 @@ def main(myArg1):
             rospy.Subscriber("/rain/status",RainMsg, callback_Mode, queue_size=1)    
             rospy.Subscriber("/leap_motion/leap_filtered",LeapMsg_orion, callback_Leap, queue_size=1)
 
+            rospy.Subscriber("/SModelRobotInput",inputMsg.SModel_robot_input, callback_Gripper, queue_size=1)    
             control_gripper()
 
         except KeyboardInterrupt:
